23b84ff1f3eeec808e075b64e2b00a5c0fd91dd8
aandrews72 <alecandrews2017@gmail.com>
2023-06-23 17:03:16 -0400

    Initialize board, fixed spot sizing and coloring issue

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import pygame
 from othello.constants import *
-
-WIDTH, HEIGHT = 1000, 800
-ROWS, COLUMNS = 8, 8
-SPOT_SIZE = WIDTH//COLUMNS
-
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
+from othello.board import Board
 
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,12 +8,18 @@ pygame.display.set_caption("Othello")
 
 def main():
     run = True
+    clock = pygame.time.Clock()
+    board = Board()
 
     while run:
+        clock.tick(60) # 60 FPS frame rate
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        board.draw_board(WINDOW)
+        pygame.display.update()
     
     pygame.quit()
 
--- othello/board.py
+++ othello/board.py
@@ -1,21 +1,26 @@
 import pygame
-from constants import *
+from othello.constants import *
 
 
 class Board:
     def __init__(self):
-        self.board = []
+        self.board = [[None for row in range(ROWS)] for col in range(COLUMNS)]
         self.selected_piece = None
         
-    def draw_cubes(self, window):
-        window.fill(GREEN)
+    def draw_board(self, window):
+        window.fill(LIGHT_GREEN)
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                pygame.draw.rect(window, GREEN, (row*SPOT_SIZE+SPOT_SIZE//20, col*SPOT_SIZE+SPOT_SIZE//20, SPOT_SIZE - SPOT_SIZE//10, SPOT_SIZE - SPOT_SIZE//10))
+
+        self.board[3][3] = WHITE
+        self.board[4][4] = WHITE
+        self.board[3][4] = BLACK
+        self.board[4][3] = BLACK
 
         for row in range(ROWS):
-            pygame.draw.rect(window, GREEN, (WIDTH, HEIGHT))
-
-        self.board[4][4] = WHITE
-        self.board[5][5] = WHITE
-        self.board[4][5] = BLACK
-        self.board[5][4] = BLACK
+            for col in range(COLUMNS):
+                if self.board[row][col] is not None:
+                    pygame.draw.circle(window, self.board[row][col], (row*SPOT_SIZE+SPOT_SIZE//2, col*SPOT_SIZE+SPOT_SIZE//2), SPOT_SIZE//2-SPOT_SIZE//5)
 
         return self.board
