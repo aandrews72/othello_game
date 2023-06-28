20a396374eacbd6f256dc2fe66fb3e815273c43a
aandrews72 <alecandrews2017@gmail.com>
2023-06-28 11:26:02 -0400

    Update main game loop to show possible moves

--- main.py
+++ main.py
@@ -3,25 +3,58 @@ from othello.constants import *
 from othello.board import Board
 
 
+
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Othello")
 
 def main():
     run = True
-    clock = pygame.time.Clock()
     board = Board()
+    current_piece = None
 
     while run:
-        clock.tick(60) # 60 FPS frame rate
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = pos[0] // SPOT_SIZE, pos[1] // SPOT_SIZE
+                if current_piece:
+                    if current_piece.move(row, col, board.board):
+                        current_piece = None
+                else:
+                    current_piece = board.board[row][col]
 
         board.draw_board(WINDOW)
+        if current_piece:
+            moves = current_piece.possible_moves(board.board)
+            for move in moves:
+                pygame.draw.circle(WINDOW, BLUE, 
+                                   (move[0]*SPOT_SIZE+SPOT_SIZE//2, move[1]*SPOT_SIZE+SPOT_SIZE//2), 25)
         pygame.display.update()
     
     pygame.quit()
 
 if __name__ == "__main__":
     main()
+
+# def main():
+#     run = True
+#     clock = pygame.time.Clock()
+#     board = Board()
+
+#     while run:
+#         clock.tick(60) # 60 FPS frame rate
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 run = False
+#                 break
+
+#         board.draw_board(WINDOW)
+#         pygame.display.update()
+    
+#     pygame.quit()
+
+# if __name__ == "__main__":
+#     main()
