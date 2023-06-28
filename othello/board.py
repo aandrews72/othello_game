7d73d81f9c7e529764700d5cd172d674b395fe5d
aandrews72 <alecandrews2017@gmail.com>
2023-06-28 11:23:55 -0400

    Integrate Piece class with Board class and update draw_board method

--- othello/board.py
+++ othello/board.py
@@ -1,26 +1,28 @@
 import pygame
 from othello.constants import *
+from othello.pieces import Piece
 
 
 class Board:
     def __init__(self):
         self.board = [[None for row in range(ROWS)] for col in range(COLUMNS)]
-        self.selected_piece = None
+        self.board[3][3] = Piece(3, 3, WHITE)
+        self.board[4][4] = Piece(4, 4, WHITE)
+        self.board[3][4] = Piece(3, 4, BLACK)
+        self.board[4][3] = Piece(4, 3, BLACK)
         
     def draw_board(self, window):
         window.fill(LIGHT_GREEN)
         for row in range(ROWS):
             for col in range(COLUMNS):
                 pygame.draw.rect(window, GREEN, (row*SPOT_SIZE+SPOT_SIZE//20, col*SPOT_SIZE+SPOT_SIZE//20, SPOT_SIZE - SPOT_SIZE//10, SPOT_SIZE - SPOT_SIZE//10))
-
-        self.board[3][3] = WHITE
-        self.board[4][4] = WHITE
-        self.board[3][4] = BLACK
-        self.board[4][3] = BLACK
-
-        for row in range(ROWS):
-            for col in range(COLUMNS):
                 if self.board[row][col] is not None:
-                    pygame.draw.circle(window, self.board[row][col], (row*SPOT_SIZE+SPOT_SIZE//2, col*SPOT_SIZE+SPOT_SIZE//2), SPOT_SIZE//2-SPOT_SIZE//5)
+                    pygame.draw.circle(window, self.board[row][col].color, (row*SPOT_SIZE+SPOT_SIZE//2, col*SPOT_SIZE+SPOT_SIZE//2), SPOT_SIZE//2-10)
+
+
+        # for row in range(ROWS):
+        #     for col in range(COLUMNS):
+        #         if self.board[row][col] is not None:
+        #             pygame.draw.circle(window, self.board[row][col], (row*SPOT_SIZE+SPOT_SIZE//2, col*SPOT_SIZE+SPOT_SIZE//2), SPOT_SIZE//2-SPOT_SIZE//5)
 
         return self.board
